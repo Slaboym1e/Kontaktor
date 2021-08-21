0519d79d0e8590d55fca2bf510457d0b4bf49650
gitu <sf/F2Ry>
2021-08-21 21:47:45 +0700

    commit db in remote sources

--- app/config.py
+++ app/config.py
@@ -1,10 +1,22 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-class configBD(object):
+class configBD_local(object):
     basedir = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join('//216.216.216.1/database', 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class configBD(object):
+    SQLALCHEMY_DIALECT  = 'mysql'
+    SQLALCHEMY_DRIVER   = 'pymysql'
+    SQLALCHEMY_USER     = 'a0529774_kontaktor'
+    SQLALCHEMY_PASSWORD = 'Tpk3b2Hf'
+    SQLALCHEMY_HOST     = '141.8.192.26'
+    SQLALCHEMY_DBNAME   = 'a0529774_kontaktor'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DIALECT+'+'+SQLALCHEMY_DRIVER+'://'+\
+                              SQLALCHEMY_USER+':'+SQLALCHEMY_PASSWORD+'@'+\
+                              SQLALCHEMY_HOST+'/'+SQLALCHEMY_DBNAME
 
 class config(object):
     UPLOAD_FOLDER = "uploads"
