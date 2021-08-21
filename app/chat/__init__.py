28b0e455e71e02ae6e42f06567d644e024515dfa
Slaboym1e <lishay.mikhaul@gmail.com>
2021-08-21 12:57:16 +0700

    comment getchats

--- app/chat/__init__.py
+++ app/chat/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 
 chat = Blueprint('chat', __name__)
+chat_api = Blueprint('chat_api',__name__)
 
 import app.chat.routes
--- app/chat/models.py
+++ app/chat/models.py
@@ -1,32 +1,32 @@
 from app import Session, app, Base, engine
-from sqlalchemy import Column, String, Integer, DateTime, BigInteger,Text
+from sqlalchemy import Column, String, Integer, DateTime, BigInteger, Text
 from flask_login import current_user
 from datetime import datetime
 
 
 class Chats(Base):
-    __tablename__= 'chats'
+    __tablename__ = 'chats'
     id = Column(BigInteger, primary_key=True)
     title = Column(String(255), unique=True, nullable=False)
 
 
 class ChatMembers(Base):
-    __tablename__= 'chat_members'
+    __tablename__ = 'chat_members'
     id = Column(BigInteger, primary_key=True)
     chat_id = Column(BigInteger, nullable=False)
-    member_id = Column(Integer, nullable= False)
+    member_id = Column(Integer, nullable=False)
 
 
 class Messages(Base):
     __tablename__ = 'chat_messages'
     id = Column(BigInteger, primary_key=True)
-    author_id =  Column(Integer, nullable=False)
+    author_id = Column(Integer, nullable=False)
     chat_id = Column(BigInteger, nullable=False)
     createtime = Column(DateTime)
     message = Column(Text)
 
     def __init__(self, message, chat_id):
-        #self.author_id = current_user.id
+        # self.author_id = current_user.id
         self.message = message
         self.createtime = datetime.now()
         self.chat_id = chat_id
