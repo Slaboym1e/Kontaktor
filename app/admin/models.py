286c7215f8c7d898099c1dc85630e028f9225e7a
Valerii <Vindsark@hotmail.com>
2021-08-21 20:19:02 +0700

    Авторизация

--- app/admin/models.py
+++ app/admin/models.py
@@ -1,7 +1,7 @@
 from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from sqlalchemy import Column, Integer, String, DateTime, Date, SmallInteger
+from sqlalchemy import Column, Integer, String, DateTime, Date, SmallInteger, Float
 from app import Base, engine,Session
 
 
@@ -23,14 +23,21 @@ class User(UserMixin, Base):
         return '<User %r>' % self.username
 
 
-# @login.user_loader
-# def load_user(user_id):
-#     return User.get(user_id)
 
 @login.user_loader
 def load_user(id):
     #return User.id(id)
     return Session.query(User).filter(User.id == id).first()
+
+
+class Area(UserMixin, Base):
+    __tablename__ = 'area'
+    id = Column(Integer, primary_key=True)
+    title   = Column(String(64), unique=False, nullable=False)
+    height   = Column(Float(12), unique=False, nullable=False)
+    width = Column(Float(12), unique=False, nullable=False)
+    user_id = Column(String(12), nullable=True)
+
 
 
 
